894b85f5f8d4f1ebe0a2d963128b2a300ba8598f
Alp Yilmaz <Hexa3333@protonmail.com>
2025-08-28 02:24:37 +0300

    Parsing tests

--- src/command.rs
+++ src/command.rs
@@ -71,10 +71,83 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_expire() {
+    fn expire_single_var() {
         let s = "EXPIRE alex 3s";
         let com = s.parse::<Command>();
 
         assert_eq!(com, Ok(Command::Expire("alex".to_string(), Duration::from_secs(3))));
+    }
+
+    #[test]
+    fn set() {
+        let key = "metanoia";
+        let value = "19";
+
+        let com = String::from("SET ") + key + " " + value;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Set("metanoia".to_string(), "19".to_string())));
+    }
+
+    #[test]
+    fn get() {
+        let key = "metanoia";
+
+        let com = String::from("GET ") + key;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Get("metanoia".to_string())));
+    }
+
+    #[test]
+    fn del() {
+        let key = "metanoia";
+
+        let com = String::from("DEL ") + key;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Del("metanoia".to_string())));
+    }
+
+    #[test]
+    fn exists() {
+        let key = "metanoia";
+
+        let com = String::from("EXISTS ") + key;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Exists("metanoia".to_string())));
+    }
+
+    #[test]
+    fn expire_multiple_var() {
+        let key = "metanoia";
+        //let duration = "1h 27m 13s".parse::<humantime::Duration>().unwrap();
+
+        let com = String::from("EXISTS ") + key + " 1h 27m 13s";
+        let com = com.parse::<Command>();
+
+        // 1h 27m 13s is 5223 seconds
+        assert_eq!(com, Ok(Command::Expire("metanoia".to_string(), Duration::from_secs(5_233))));
+    }
+
+    #[test]
+    fn incr() {
+        let key = "metanoia";
+
+        let com = String::from("INCR ") + key;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Incr("metanoia".to_string())));
+    }
+
+    #[test]
+    fn decr() {
+        let key = "metanoia";
+
+        let com = String::from("DECR ") + key;
+        let com = com.parse::<Command>();
+
+        assert_eq!(com, Ok(Command::Decr("metanoia".to_string())));
     }
 }
